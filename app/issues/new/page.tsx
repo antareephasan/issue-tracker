965169e4cae6e5cda45f40556335a72c779f3e69
antareephasan <antareephasan@gmail.com>
2024-02-11 04:23:54 +0600

    move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -20,6 +21,18 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema)
     });
     const [error, setError] = useState('');
+    const [isSubmitting, setSubmitting] = useState(false);
+
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues')
+        } catch (error) {
+            setSubmitting(false);
+            setError('An unexpected error occured.');
+        }
+    })
     return (
         <div className='max-w-xl'>
             {error && (
@@ -29,14 +42,7 @@ const NewIssuePage = () => {
             )}
             <form
                 className='space-y-3'
-                onSubmit={handleSubmit(async (data) => {
-                    try {
-                        await axios.post('/api/issues', data);
-                        router.push('/issues')
-                    } catch (error) {
-                        setError('An unexpected error occured.');
-                    }
-                })}>
+                onSubmit={onSubmit}>
                 <TextField.Root>
                     <TextField.Input placeholder="Title" {...register('title')} />
                 </TextField.Root>
@@ -48,7 +54,7 @@ const NewIssuePage = () => {
                     }
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
             </form>
         </div>
 
